32f7b171a418d731113045ee5a4782ae651ddaeb
Fred Erik <stammix@web.de>
2016-10-16 23:06:27 +0200

    setting persisting Storage for Settings

--- BBallStats/SettingsViewController.swift
+++ BBallStats/SettingsViewController.swift
@@ -10,26 +10,52 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    var collectForBothTeams = false
+    var homeTeamColor = UIColor.white
+    var opponentTeamColor = UIColor.black
+    
+  
+    @IBOutlet weak var SwitchState: UISwitch!
+    @IBAction func OneOrBothTeamsSwitch(_ sender: UISwitch) {
+        if SwitchState.isOn {
+            collectForBothTeams = true
+        } else {
+            collectForBothTeams = false
+        }
+    
+    }
+    @IBAction func tippOffButton(_ sender: AnyObject) {
+        UserDefaults.standard.set("\(collectForBothTeams)", forKey: "OneOrTwoTeams")
+        UserDefaults.standard.set("\(homeTeamColor)", forKey: "homeTeamColor")
+        UserDefaults.standard.set("\(opponentTeamColor)", forKey: "opponentTeamColor")
+         self.performSegue(withIdentifier: "SettingsToGameSegue", sender: self)
+    }
+    
+    @IBAction func HomeTeamBlack(_ sender: AnyObject) {
+        homeTeamColor = UIColor.black
+    }
+    @IBAction func HomeTeamBlue(_ sender: AnyObject) {
+        homeTeamColor = UIColor.blue
+    }
+    
+    @IBAction func HomeTeamRed(_ sender: AnyObject) {
+        homeTeamColor = UIColor.red
+    }
+    
+    @IBAction func OpponentTeamBlack(_ sender: UIButton) {
+        opponentTeamColor = UIColor.black
+    }
+    @IBAction func OpponentTeamBlue(_ sender: UIButton) {
+         opponentTeamColor = UIColor.blue
+    }
+    @IBAction func OpponentTeamRed(_ sender: UIButton) {
+         opponentTeamColor = UIColor.red
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
